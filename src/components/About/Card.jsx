8e1ab4613b9da19e754a97f9d860523585b80364
Jishnu Khargharia <jishnukhargharia11@gmail.com>
2024-09-10 21:06:10 +0530

    add more profile icons

--- src/components/About/Card.jsx
+++ src/components/About/Card.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import test from '../../Images/text .png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons/faXTwitter';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 
 const Card = ({ name, position, linkedin }) => {
     return (
@@ -10,7 +12,7 @@ const Card = ({ name, position, linkedin }) => {
                 <img
                     src={test}
                     alt="Laptop"
-                    className="h-[300px] w-full rounded-t-md object-cover"
+                    className="h-[300px] rounded-t-md object-cover"
                 />
                 <div className="p-4 flex flex-col justify-center items-center">
                     <h1 className="text-2xl font-semibold">{name}</h1>
@@ -18,10 +20,22 @@ const Card = ({ name, position, linkedin }) => {
                         {position}
                     </p>
                     <a href={linkedin} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon
-                            icon={faLinkedin}
-                            className="h-7 mt-2 hover:text-blue-700 hover:scale-110 transition-transform duration-300"
-                        />
+                        <div>
+                            <FontAwesomeIcon
+                                icon={faLinkedin}
+                                className="h-7 mt-2 px-2 hover:text-blue-700 hover:scale-110 transition-transform duration-300"
+                            />
+
+                            <FontAwesomeIcon
+                                icon={faXTwitter}
+                                className="h-7 mt-2 px-2 hover:text-blue-700 hover:scale-110 transition-transform duration-300"
+                            />
+
+                            <FontAwesomeIcon
+                                icon={faGithub}
+                                className="h-7 mt-2 px-2 hover:text-blue-700 hover:scale-110 transition-transform duration-300"
+                            />
+                        </div>
                     </a>
                 </div>
             </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -68,6 +68,14 @@ const Header = () => {
                                         CONTACT US
                                     </NavLink>
                                 </li>
+                                <li>
+                                <NavLink to="register"
+                                        className={({ isActive }) =>
+                                            `block py-2 pr-4 pl-3 duration-200  ${isActive ? 'text-red-600' : 'text-white'}  border-b  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-600 lg:p-0`
+                                        }>
+                                        REGISTER
+                                    </NavLink>
+                                </li>
                             </ul>
                         </div>
 
