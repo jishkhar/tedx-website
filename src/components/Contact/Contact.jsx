05513d5b4f38015d52b4ddee734b4e7314f9fd7e
Jishnu Khargharia <jishnukhargharia11@gmail.com>
2024-09-10 22:43:09 +0530

    add animation in contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,11 +5,20 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
+import { motion } from 'framer-motion'
+import { fadeIn3, fadeIn4 } from '../../variants.js';
+
 const Contact = () => {
   return (
     <>
       <div className='flex justify-between h-[85vh] w-[75%] mx-auto my-20'>
-        <div id="map" className='mt-4'>
+        <motion.div
+          variants={fadeIn3("right", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true }}
+
+          id="map" className='mt-4'>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.373143920216!2d77.12574074408558!3d13.32704926577623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb02e826fb691ef%3A0xe78608813c36f380!2sSiddaganga%20Institute%20Of%20Technology!5e0!3m2!1sen!2sin!4v1725982925575!5m2!1sen!2sin" width="750" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
@@ -34,28 +43,40 @@ const Contact = () => {
             </div>
           </div>
 
-        </div>
+        </motion.div>
 
-        <div className='py-10 h-fit border-2 border-red-600 rounded-xl'>
+        <motion.div
+          variants={fadeIn4("left", 0.05)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true }}
+
+          className='py-10 h-fit border-2 border-red-600 rounded-xl'>
           <form className='h-fit py-7 px-10'>
             <div><h1 className='text-6xl mb-5'><span className='text-red-600'>Contact</span> Us</h1></div>
 
             <div className='flex flex-col'>
               <label className='text-[1.2rem] sm:text-[1.5rem] md:text-[1.8rem] mb-3'>Name</label>
-              <input className="text-black mb-5 h-10 w-[25vw] text-lg sm:text-xl md:text-2xl rounded-sm" type="text" />
+              <input className="text-black mb-5 h-10 w-[25vw] text-lg sm:text-xl md:text-2xl rounded-md" type="text" />
 
 
 
               <label className='text-[1.2rem] sm:text-[1.5rem] md:text-[1.8rem] mb-3'>Email</label>
-              <input className="text-black mb-5 h-10 text-lg sm:text-xl md:text-2xl rounded-sm" type="text" />
+              <input className="text-black mb-5 h-10 text-lg sm:text-xl md:text-2xl rounded-md" type="text" />
 
 
 
               <label className='text-[1.2rem] sm:text-[1.5rem] md:text-[1.8rem] mb-3'>Message</label>
-              <textarea className="text-black mb-14 h-32 text-lg sm:text-xl md:text-2xl rounded-sm"></textarea>
+              <textarea className="text-black mb-14 h-32 text-lg sm:text-xl md:text-2xl rounded-md"></textarea>
+
+              <input
+                className="cursor:pointer rounded-lg h-14 bg-[rgb(18,18,18)] text-[1.4rem] sm:text-[1.6rem] md:text-[1.8rem] cursor-pointer text-white transition-colors duration-300 hover:bg-[rgb(30,30,30)] hover:text-gray-300"
+                type="submit"
+                value="Submit"
+              />
             </div>
           </form>
-        </div>
+        </motion.div>
       </div>
     </>
   )
--- src/variants.js
+++ src/variants.js
@@ -40,3 +40,47 @@ export const fadeIn2 = (direction, delay) => {
         }
     }
 }
+
+
+
+
+export const fadeIn3 = (direction, delay) => {
+    return{
+        hidden: {
+            y: direction === 'up' ? 10 : direction === 'down' ? -10 : 0,
+            x: direction === 'left' ? 900 : direction === 'right' ? -900 : 0,
+        },
+        show: {
+            y: 0,
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'tween',
+                duration: 1.5,
+                delay: delay,
+                ease: [0.25, 0.25, 0.25, 0.75],
+            }
+        }
+    }
+}
+
+
+export const fadeIn4 = (direction, delay) => {
+    return{
+        hidden: {
+            y: direction === 'up' ? 10 : direction === 'down' ? -10 : 0,
+            x: direction === 'left' ? 780 : direction === 'right' ? -200 : 0,
+        },
+        show: {
+            y: 0,
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'tween',
+                duration: 1.5,
+                delay: delay,
+                ease: [0.25, 0.25, 0.25, 0.75],
+            }
+        }
+    }
+}
