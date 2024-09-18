fdae43e7ad8669f5d5a0a02c279b5497aa247bc0
Jishnu Khargharia <jishnukhargharia11@gmail.com>
2024-09-18 21:13:06 +0530

    testing testing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -17,15 +17,15 @@ const Contact = () => {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: true }}
-          id="map" 
+          id="map"
           className='mt-4 bg-[rgb(30, 30, 30)] order-2 lg:order-1'
         >
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.373143920216!2d77.12574074408558!3d13.32704926577623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb02e826fb691ef%3A0xe78608813c36f380!2sSiddaganga%20Institute%20Of%20Technology!5e0!3m2!1sen!2sin!4v1725982925575!5m2!1sen!2sin" 
-            width="100%" 
-            height="450" 
-            allowFullScreen="" 
-            loading="lazy" 
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.373143920216!2d77.12574074408558!3d13.32704926577623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb02e826fb691ef%3A0xe78608813c36f380!2sSiddaganga%20Institute%20Of%20Technology!5e0!3m2!1sen!2sin!4v1725982925575!5m2!1sen!2sin"
+            width="100%"
+            height="450"
+            allowFullScreen=""
+            loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
 
